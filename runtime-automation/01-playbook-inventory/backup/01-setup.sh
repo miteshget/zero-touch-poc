75c1b7cddfbebbf1c815f3ee6d0254cc4ef6b595
Mitesh Sharma <mitsharm@redhat.com>
2024-07-12 14:25:06 +0530

    update

--- runtime-automation/01-playbook-inventory/backup/01-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f "./${MODULE_DIR}/playbooks/${MODULE_STAGE}.yml" ]
-  then
-    /opt/app-root/bin/ansible-playbook ./${MODULE_DIR}/playbooks/${MODULE_STAGE}.yml
-else
-  echo "Setup doesn't exist, skipping"
-fi
--- runtime-automation/01-playbook-inventory/backup/validation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f "./${MODULE_DIR}/playbooks/${MODULE_STAGE}.yml" ]
-  then
-    /opt/app-root/bin/ansible-playbook ./${MODULE_DIR}/playbooks/${MODULE_STAGE}.yml
-else
-  echo "Validation doesn't exist, skipping"
-fi
